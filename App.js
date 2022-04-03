e0f9384ac4b6de2464b5dc6ff6ebe3eb479139a2
JonasFiechter <jonasrfiechter@gmail.com>
2022-04-03 16:03:52 -0300

    Last commit before styling!

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  SafeAreaView,
+  TouchableOpacity, 
+  Button, 
+  Alert,
+  Platform,
+  StatusBar
+} from 'react-native';
 
 export default function App() {
+
+  function my_press() {Alert.alert('Clicked item', 'Message of alert')}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={my_press}>
+        <Image style={styles.image} source={{uri: 'https://picsum.photos/200'}}/>
+      </TouchableOpacity>
+      <Text onPress={my_press} style={styles.text}>This is the begining</Text>
+      <Button onPress={my_press} title='Click me!'/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
+  text: {
+    fontSize: 30
+  },
+  image: {
+    width: 300,
+    height: 300,
+
+  }
 });
